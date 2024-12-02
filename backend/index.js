6bfb3e85b58572a5575cf18151ebf92840d8bb7b
Andrew Malone <mandrew434@gmail.com>
2024-12-02 14:03:28 -0700

    Add bootstrap to landing and change css

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,11 @@ let app = express(); // app is now an object of express type. App is variable of
 
 let path = require('path'); // access to the path 
 
-// lets you import your .env variables from config.js
-const config = require('./config/config'); 
+// // lets you import your .env variables from config.js
+// const config = require('./config/config'); 
 
-// Importing Users model used to run CRUD operations on db table for user table
-const Users = require("./models/users");
+// // Importing Users model used to run CRUD operations on db table for user table
+// const Users = require("./models/users");
 
 let port = 5001
 
@@ -20,11 +20,22 @@ app.use(express.urlencoded( {extended: true} )) //determines how html is receive
 // This is an object literal. Basically a dictionary
 
 
-
 app.set("view engine", "ejs") //shows what view engine we are using 
+
 app.set("views", path.join(__dirname, "../frontend/views")) //This is telling the server that we are going to start using certain views
 
 app.use(express.urlencoded({extended: true})); //allows us to get data out of the request.body
 
 // Serve static files from the 'CSS' directory
 app.use('/CSS', express.static(path.join(__dirname, '../frontend/css')));
+
+// Serve static files from the frontend folder
+app.use(express.static('frontend'));
+
+// Routes and other middleware
+app.get('/', (req, res) => {
+  res.render('index');  // Or your view rendering logic
+});
+
+// app listening
+app.listen(port, () => console.log("Express App has started and server is listening!"));
